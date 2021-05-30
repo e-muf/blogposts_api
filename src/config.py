1ddbbf3938a408e92ed778ce9e79998e5f14bd88
Emanuel Flores <emnl.fmtz@gmail.com>
2021-05-30 14:34:57 -0500

    project structured and configuration

---
+++ src/config.py
@@ -0,0 +1,28 @@
+# /src/config.py
+
+import os
+
+class Config(object):
+  """
+  Base config
+  """
+  TESTING = False
+  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+  """
+  Development environment configuration
+  """
+  DEBUG = True
+  
+class ProductionConfig(Config):
+  """
+  Production environment configurations
+  """
+  DEBUG = False
+
+app_config = {
+  'development': DevelopmentConfig,
+  'production': ProductionConfig,
+}
